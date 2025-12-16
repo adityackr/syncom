09d8adcbbd4cd69d2de13d0ef03988b668434df2
adityackr <aditya09me@gmail.com>
2025-12-16 14:14:44 +0600

    feat: Reset message input form and editor state after successful message submission and add ESLint ignore for global ORPC client.

--- app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/message/message-input-form.tsx
+++ app/(dashboard)/workspace/[workspaceId]/channel/[channelId]/_components/message/message-input-form.tsx
@@ -11,7 +11,7 @@ import {
 import { orpc } from '@/lib/orpc';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { MessageComposer } from './message-composer';
@@ -22,6 +22,7 @@ type MessageInputFormProps = {
 
 export const MessageInputForm: FC<MessageInputFormProps> = ({ channelId }) => {
 	const queryClient = useQueryClient();
+	const [editorKey, setEditorKey] = useState(0);
 
 	const form = useForm<CreateMessage>({
 		resolver: zodResolver(CreateMessageSchema),
@@ -37,6 +38,10 @@ export const MessageInputForm: FC<MessageInputFormProps> = ({ channelId }) => {
 				queryClient.invalidateQueries({
 					queryKey: orpc.message.list.key(),
 				});
+
+				form.reset({ channelId, content: '' });
+				setEditorKey((prev) => prev + 1);
+
 				return toast.success('Message sent successfully');
 			},
 			onError: (error) => {
@@ -62,6 +67,7 @@ export const MessageInputForm: FC<MessageInputFormProps> = ({ channelId }) => {
 						<FormItem>
 							<FormControl>
 								<MessageComposer
+									key={editorKey}
 									onSubmit={() => handleSubmit(form.getValues())}
 									value={field.value || ''}
 									onChange={field.onChange}
--- lib/orpc.server.ts
+++ lib/orpc.server.ts
@@ -3,4 +3,7 @@ import 'server-only';
 import { router } from '@/app/router';
 import { createRouterClient } from '@orpc/server';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+
 globalThis.$client = createRouterClient(router, {});
